b9393002c77a677da3d1cda5c45f58ed6a7fa8e4
Alex <Cevalloa@gmail.com>
2026-07-18 17:12:43 -0400

    Initial Commit

---
+++ OnlineShopping_SwiftUI/OnlineShopping_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopping_SwiftUIApp.swift
+//  OnlineShopping_SwiftUI
+//
+//  Created by Alex Cevallos on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopping_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnlineShopping_SwiftUITests/OnlineShopping_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopping_SwiftUITests.swift
+//  OnlineShopping_SwiftUITests
+//
+//  Created by Alex Cevallos on 7/18/26.
+//
+
+import Testing
+@testable import OnlineShopping_SwiftUI
+
+struct OnlineShopping_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
